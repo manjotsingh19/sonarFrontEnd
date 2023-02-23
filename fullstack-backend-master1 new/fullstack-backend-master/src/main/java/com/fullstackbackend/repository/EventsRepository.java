af74eebca79f60611817f1e144b9161f17d3d150
PrashantSahu29 <124778228+PrashantSahu29@users.noreply.github.com>
2023-02-23 13:16:02 +0530

    Delete fullstack-backend-master1 new/fullstack-backend-master directory

--- fullstack-backend-master1 new/fullstack-backend-master/src/main/java/com/fullstackbackend/repository/EventsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fullstackbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fullstackbackend.model.Events;
-
-public interface EventsRepository extends JpaRepository<Events,Long>{
-
-}
--- fullstack-backend-master1 new/fullstack-backend-master/src/main/java/com/fullstackbackend/repository/TeamFormRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fullstackbackend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fullstackbackend.model.TeamForm;
-
-public interface TeamFormRepository extends JpaRepository<TeamForm, Integer> {
-
-}
