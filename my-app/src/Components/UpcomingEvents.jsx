3567558810145272e43bb547606b36d6b6d3902c
va-incedo <vaibhav.agarwal2@incedoinc.com>
2023-02-25 17:20:54 +0530

    update in carousal

--- my-app/src/Components/UpcomingEvents.jsx
+++ my-app/src/Components/UpcomingEvents.jsx
@@ -1,36 +1,171 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MDBCarousel, MDBCarouselItem } from "mdb-react-ui-kit";
+import Slider from "react-slick";
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import p1 from "./images/1.jpeg";
+import p2 from "./images/2.jpeg";
+import p3 from "./images/3.png";
+import p4 from "./images/4.png";
+import p5 from "./images/5.jpeg";
+import p6 from "./images/6.jpeg";
+import p7 from "./images/7.jpeg";
+import p8 from "./images/8.jpeg";
+
+const Card = ({
+  name,
+  description,
+  image,
+  atwitter,
+  alinkedin,
+  agithub,
+  designation,
+}) => {
+  return (
+    <div class="" style={{ overflow: "hidden", margin: "0 10px" }}>
+      <div class="team-item">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={image} class="team-img" alt="pic" />
+        </div>
+
+        <h3>{name}</h3>
+
+        <div class="team-info">
+          <p>{designation}</p>
+        </div>
+        <div class="tagline">
+          <p>{description}</p>
+        </div>
+
+        <div class="team-icon">
+          <div>
+            <a href={atwitter} class="twitter">
+              <FaTwitter />
+            </a>
+          </div>
+          <div>
+            <a href={agithub} class="github">
+              <FaGithub />
+            </a>
+          </div>
+          <div>
+            <a href={alinkedin} class="linkedin">
+              <FaLinkedin />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function App() {
+  const slider = useRef(null);
+
+  const settings = {
+    centerMode: true,
+    // draggable: true,
+    centerPadding: "20px",
+    pauseOnHover: true,
+    infinite: true,
+    // swipeToSlide: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+
+    focusOnSelect: true,
+    // fade: true,
+  };
+
+  const data = [
+    {
+      name: "Mahak Jain",
+      image: p1,
+      designation: "Quality Assurance Engineer",
+      alinkedin: "https://www.linkedin.com/in/mahak-jain-7a4488120/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description:
+        "Responsible for the testing and automation of the application",
+    },
+    {
+      name: "Manjot Singh",
+      image: p2,
+      designation: "Quality Assurance Engineer",
+      alinkedin: "https://www.linkedin.com/in/manjot-singh-5297171b4/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description:
+        "Responsible for the testing and automation of the application",
+    },
+    {
+      name: "Mritunjay Verma",
+      image: p3,
+      designation: "Software Developer Engineer",
+      alinkedin: "https://www.linkedin.com/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description: "Responsible for the backend development of the application",
+    },
+    {
+      name: "Nikunj Gupta",
+      image: p4,
+      designation: "Software Developer Engineer",
+      alinkedin: "https://www.linkedin.com/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description: "Responsible for the backend development of the application",
+    },
+    {
+      name: "Nimish Chourasia",
+      image: p5,
+      designation: "Software Developer Engineer",
+      alinkedin: "https://www.linkedin.com/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description: "Responsible for the backend development of the application",
+    },
+    {
+      name: "Prashant sahu",
+      image: p6,
+      designation: "Software Developer Engineer",
+      alinkedin: "https://www.linkedin.com/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description:
+        "Responsible for the frontend development of the application",
+    },
+    {
+      name: "Vaibhav Agarwal",
+      image: p7,
+      designation: "Software Developer Engineer",
+      alinkedin: "https://www.linkedin.com/in/va29/",
+      atwitter: "https://twitter.com/va2905",
+      agithub: "https://github.com/va-incedo",
+      description: "Responsible for the backend development of the application",
+    },
+    {
+      name: "Yashi Gupta",
+      image: p8,
+      designation: "Software Developer Engineer",
+      alinkedin: "https://www.linkedin.com/in/yashi-gupta-6b2bb3228/",
+      atwitter: "https://twitter.com/",
+      agithub: "https://github.com/",
+      description: "Responsible for the backend development of the application",
+    },
+  ];
   return (
-    <MDBCarousel showControls showIndicators>
-      <MDBCarouselItem
-        className="w-100 d-block"
-        itemId={1}
-        src="https://mdbootstrap.com/img/new/slides/041.jpg"
-        alt="..."
-      >
-        <h5>First slide label</h5>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      </MDBCarouselItem>
-      <MDBCarouselItem
-        className="w-100 d-block"
-        itemId={2}
-        src="https://mdbootstrap.com/img/new/slides/042.jpg"
-        alt="..."
-      >
-        <h5>Second slide label</h5>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      </MDBCarouselItem>
-      <MDBCarouselItem
-        className="w-100 d-block"
-        itemId={3}
-        src="https://mdbootstrap.com/img/new/slides/043.jpg"
-        alt="..."
-      >
-        <h5>Third slide label</h5>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      </MDBCarouselItem>
-    </MDBCarousel>
+    <Slider {...settings} ref={slider}>
+      {data.map((card, i) => (
+        <Card {...card} />
+      ))}
+    </Slider>
   );
 }
