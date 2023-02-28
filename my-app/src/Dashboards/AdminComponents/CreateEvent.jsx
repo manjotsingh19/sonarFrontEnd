90f14551b5415a9d41ffcfbeea81b782b20f5fcb
va-incedo <vaibhav.agarwal2@incedoinc.com>
2023-02-28 22:58:02 +0530

    changes

--- my-app/src/Dashboards/AdminComponents/CreateEvent.jsx
+++ my-app/src/Dashboards/AdminComponents/CreateEvent.jsx
@@ -20,8 +20,7 @@ function CreateEvent() {
 
     useEffect(() => {
         // console.log(errors);
-        if (Object.keys(errors).length === 0 && submitted) 
-         {
+        if (Object.keys(errors).length === 0 && submitted) {
             // console.log(event);
             axios.post('/event', event)
                 .then((response) => {
@@ -29,8 +28,8 @@ function CreateEvent() {
                         'Great',
                         'Event added successfully!',
                         'success'
-                      )
-                      setEvent({...event,eventName: '',startDate: '',endDate: '',description: ''});
+                    )
+                    setEvent({ ...event, eventName: '', startDate: '', endDate: '', description: '' });
                 }, (error) => {
                     console.log(error);
                     Swal.fire({
@@ -38,14 +37,14 @@ function CreateEvent() {
                         title: 'Oops...',
                         text: 'Something went wrong!',
                         // footer: '<a href="">Why do I have this issue?</a>'
-                      })
+                    })
                 });
 
         }
-        else{
+        else {
             setSubmited(false);
         }
-    }, [errors,submitted]);
+    }, [errors, submitted]);
 
 
     const handleSubmit = (e) => {
@@ -91,7 +90,7 @@ function CreateEvent() {
 
                         <MDBRow className='align-items-center pt-0 '>
                             <MDBCol md='4' >
-                                <MDBInput id="eventName" value={event.eventName} onChange={(e) => handleInput(e)} wrapperClass='mb-2' required className='col-md-4' label='eventName' size='md' type='text' />
+                                <MDBInput id="eventName" value={event.eventName} onChange={(e) => handleInput(e)} wrapperClass='mb-2' required className='col-md-4' label='Event Name' size='md' type='text' />
                                 <p style={myStyle}>{errors.eventName}</p>
                             </MDBCol>
 
@@ -100,6 +99,7 @@ function CreateEvent() {
                                 <div className="" style={{ bordeRadius: "50px", marginBottom: "15px" }}>
                                     <Form.Control
                                         placeholder="Start date"
+                                        label="Start Date"
                                         type="date"
                                         id="startDate"
                                         value={event.startDate}
--- my-app/src/Dashboards/PanelistDashboard/Panelist.jsx
+++ my-app/src/Dashboards/PanelistDashboard/Panelist.jsx
@@ -1,5 +1,11 @@
 // import "react-datepicker/dist/react-datepicker.css";
-import { MDBRow, MDBCol, MDBContainer, MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+import {
+  MDBRow,
+  MDBCol,
+  MDBContainer,
+  MDBCard,
+  MDBCardBody,
+} from "mdb-react-ui-kit";
 import "./Panelist.css";
 import PanelistModal from "./PanelistModal";
 import { useState, useEffect } from "react";
@@ -7,42 +13,31 @@ import axios from "axios";
 import Navbar from "../../Components/Navbar";
 import Timer from "../JugdeComponents/Timer";
 
-
 const Card = ({ teamObj }) => {
   // console.log(teamObj);
 
-  const { teamId, teamName, idea } = teamObj
+  const { teamId, teamName, idea } = teamObj;
   return (
     <div className="teamCard">
       <MDBCard>
         <MDBCardBody>
           <MDBRow>
-            <MDBCol md="6">
-              <h6 className="fw-bold">Team ID</h6>
-            </MDBCol>
-            <MDBCol md="6">
-              <h6 className="fw-bold">Team Name</h6>
-            </MDBCol>
+            <MDBCol md="6"><h6 className="fw-bold">Team ID</h6></MDBCol>
+            <MDBCol md="6"><h6 className="fw-bold">Team Name</h6></MDBCol>
           </MDBRow>
           <MDBRow>
-            <MDBCol md="6">
-              <h4 className="fw-bold">{teamId}</h4>
-            </MDBCol>
-            <MDBCol md="6">
-              <h4 className="fw-bold">{teamName}</h4>
-            </MDBCol>
+            <MDBCol md="6"><h4 className="fw-bold">{teamId}</h4></MDBCol>
+            <MDBCol md="6"><h4 className="fw-bold">{teamName}</h4></MDBCol>
           </MDBRow>
           <MDBRow>
-            <MDBCol md="12">
-              <h6 className="fw-bold">Problem Statement</h6>
-            </MDBCol>
+            <MDBCol md="12"><h6 className="fw-bold">Problem Statement</h6></MDBCol>
           </MDBRow>
           <MDBRow>
-            <MDBCol md="12">
-              <p className="fw-medium">{idea?.problemStatement}</p>
-            </MDBCol>
+            <MDBCol md="12"><p className="fw-medium">{idea?.problemStatement}</p></MDBCol>
           </MDBRow>
-          <PanelistModal item={{ teamId, teamName, statement: idea.problemStatement, description: idea.description, teamObj }} />
+          <PanelistModal
+            item={{ teamId, teamName, statement: idea.problemStatement, description: idea.description, teamObj, }}
+          />
           {/* <PanelistModal userObj = {teamObj}/> */}
         </MDBCardBody>
       </MDBCard>
@@ -50,24 +45,23 @@ const Card = ({ teamObj }) => {
   );
 };
 function Panelist() {
-
   const [team, setTeam] = useState([]);
   useEffect(() => {
-
-    axios.get('/getTeam')
-      .then(response => {
+    axios.get("/getTeam").then(
+      (response) => {
         // console.log(response.data);
         setTeam(response.data);
         // console.log("this is team table" + team);
-      }, (error) => {
+      },
+      (error) => {
         console.log(error);
-      });
+      }
+    );
   }, [team]);
 
-
   const filtered = team.filter((value, index) => {
-    return value.status === "pending"
-  })
+    return value.status === "pending";
+  });
 
   return (
     <>
