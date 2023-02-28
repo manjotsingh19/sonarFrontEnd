fcff03b49be539f7213ebe30bca2ea1d117723ca
va-incedo <vaibhav.agarwal2@incedoinc.com>
2023-02-28 23:23:06 +0530

    cardupdate

--- my-app/src/Components/UpcomingEvents.jsx
+++ my-app/src/Components/UpcomingEvents.jsx
@@ -11,7 +11,7 @@ const Card = ({ description, endDate, eventId, eventName, startDate, }) => {
   return (
     <div className="" style={{ overflow: "hidden", margin: "-20px 10px" }}>
       <div className="team-item" style={{
-        border: "0px solid black", borderRadius: "30px",background: "rgb(210,192,165)",
+        border: "0px solid black", borderRadius: "30px", background: "rgb(210,192,165)",
         background: "linear-gradient(90deg, rgba(210,192,165,1) 8%, rgba(172,168,237,1) 52%, rgba(56,219,252,1) 94%)",
         height: "50vh"
       }}>
@@ -47,7 +47,7 @@ export default function App() {
   const slider = useRef(null);
 
   const [event, setEvent] = useState([]);
-  
+
   useEffect(() => {
     axios.get("/getEvent").then(
       (response) => {
@@ -58,16 +58,16 @@ export default function App() {
         console.log(error);
       }
     );
-  
+
   }, []);
 
-  const settings = { centerMode: true, draggable: true, centerPadding: "20px", pauseOnHover: true, infinite: true, swipeToSlide: true, slidesToShow: event?.length, slidesToScroll: 1, autoplay: true, speed: 1000, autoplaySpeed: 2000, focusOnSelect: true, };
+  const settings = { centerMode: true, draggable: true, centerPadding: "20px", pauseOnHover: true, infinite: true, swipeToSlide: true, slidesToShow: (event?.length < 4) ? event?.length : 4, slidesToScroll: 1, autoplay: true, speed: 1000, autoplaySpeed: 2000, focusOnSelect: true, };
 
 
 
   return (
     <>
-      {event?.length>0 && (
+      {event?.length > 0 && (
         <div className="bg-color">
           <h2 className="fw-bold mb-2 pb-2 pb-md-0 mb-md-5 text-center">Upcoming events</h2>
           <Slider {...settings} ref={slider}>
